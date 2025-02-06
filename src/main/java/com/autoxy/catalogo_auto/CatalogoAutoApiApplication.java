7ce756f20731819834128462516f0f5899ea9d26
Giulio <giulio.marciante@hotmail.com>
2025-02-06 13:00:27 +0100

    first commit

---
+++ src/main/java/com/autoxy/catalogo_auto/CatalogoAutoApiApplication.java
@@ -0,0 +1,13 @@
+package com.autoxy.catalogo_auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoAutoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoAutoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autoxy/catalogo_auto/CatalogoAutoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoxy.catalogo_auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoAutoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
