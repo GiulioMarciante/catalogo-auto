db9b73ed0d1b36c1f8fbb3655f7fa87a245f568e
Giulio <giulio.marciante@hotmail.com>
2025-02-09 18:41:24 +0100

    feat: creato e documentato con Javadoc AutoService
    - Creata l'interfaccia `AutoService` per la gestione delle automobili
    - Aggiunta documentazione Javadoc per la classe e i metodi
    - Implementate operazioni CRUD tramite DTO

---
+++ src/main/java/com/autoxy/catalogo_auto/Service/AutoService.java
@@ -0,0 +1,49 @@
+package com.autoxy.catalogo_auto.Service;
+
+import com.autoxy.catalogo_auto.DTO.AutoRequestDTO;
+import com.autoxy.catalogo_auto.DTO.AutoResponseDTO;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+/**
+ * Service layer per la gestione delle automobili.
+ * Offre operazioni CRUD tramite DTO.
+ */
+public interface AutoService {
+    /**
+     * Recupera tutte le auto
+     * @return Lista di AutoResponseDTO
+     */
+    List<AutoResponseDTO> findAll();
+
+    /**
+     * Cerca un'auto per ID
+     * @param id Identificativo dell'auto
+     * @return Optional contenente l'auto se trovata
+     */
+    Optional<AutoResponseDTO> findById(Long id);
+
+    /**
+     * Salva una nuova auto
+     * @param autoRequestDTO DTO con i dati dell'auto
+     * @return AutoResponseDTO con i dati salvati
+     */
+    AutoResponseDTO save(AutoRequestDTO autoRequestDTO);
+
+    /**
+     * Aggiorna un'auto esistente
+     * @param id Identificativo auto da aggiornare
+     * @param autoRequestDTO DTO con i nuovi dati
+     * @return AutoResponseDTO aggiornata
+     * @throws NoSuchElementException se l'auto non esiste
+     */
+    AutoResponseDTO update(Long id, AutoRequestDTO autoRequestDTO);
+
+    /**
+     * Elimina un'auto per ID
+     * @param id Identificativo auto da eliminare
+     */
+    void deleteById(Long id);
+}
