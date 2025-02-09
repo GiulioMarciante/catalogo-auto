247d5100b7260d9e2e396f0c7a1cdd60e003c9e9
Giulio <giulio.marciante@hotmail.com>
2025-02-09 18:15:00 +0100

    feat: creato e documentato enumeratore StatoAuto
    - Creato l'enumeratore `StatoAuto` con i valori DISPONIBILE e VENDUTA
    - Aggiunta documentazione Javadoc per l'enumeratore e i suoi campi

---
+++ src/main/java/com/autoxy/catalogo_auto/Enum/StatoAuto.java
@@ -0,0 +1,12 @@
+package com.autoxy.catalogo_auto.Enum;
+
+/**
+ * Stati possibili di un'automobile
+ */
+public enum StatoAuto {
+    /** Auto disponibile per la vendita */
+    DISPONIBILE,
+
+    /** Auto già venduta */
+    VENDUTA
+}
