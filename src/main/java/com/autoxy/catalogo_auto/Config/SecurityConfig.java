8417a9edf50f125575a38875eba16fac345b7a9d
Giulio <giulio.marciante@hotmail.com>
2025-02-09 16:49:40 +0100

    chore: aggiornato pom.xml, application.properties e SecurityConfig
    - Aggiornato pom.xml con le dipendenze necessarie
    - Configurato application.properties per utilizzare il database H2
    - Creato SecurityConfig per avere i permessi per la console H2

--- src/main/java/com/autoxy/catalogo_auto/Config/SecurityConfig.java
+++ src/main/java/com/autoxy/catalogo_auto/Config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.autoxy.catalogo_auto;
+package com.autoxy.catalogo_auto.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
